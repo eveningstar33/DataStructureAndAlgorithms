87c53a0310bee752ad45101c7fa2b5e160430a63
“Dragos <contact@usdgadget.com>
2019-02-04 19:24:30 +0200

    Added circular linked list

---
+++ src/ds/circularlinkedlists/CircularLinkedList.java
@@ -0,0 +1,61 @@
+package ds.circularlinkedlists;
+
+public class CircularLinkedList {
+
+	private Node first;
+	private Node last;
+	
+	public CircularLinkedList() {
+		first = null;
+		last = null;
+	}
+	
+	public boolean isEmpty() {
+		return (first == null);
+	}
+	
+	public void insertFirst(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if (isEmpty()) {
+			last = newNode;
+		}
+		
+		newNode.next = first;
+		first = newNode;
+	}
+	
+	public void insertLast(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if (isEmpty()) {
+			first = newNode;
+		} else {
+			last.next = newNode;
+			last = newNode;
+		}
+	}
+	
+	public int deleteFirst() {
+		int temp = first.data;
+		
+		if (first.next == null) {
+			last = null;
+		}
+		
+		first = first.next;
+		return temp;
+	}
+	
+	public void displayList() {
+		System.out.println("List (first --> last) ");
+		Node currentNode = first;
+		while (currentNode != null) {
+			currentNode.displayNode();
+			currentNode = currentNode.next;
+		}
+		System.out.println(); 
+	}
+}
