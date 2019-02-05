1af871476384471ecf2b3d8f4d766db9e53d8941
“Dragos <contact@usdgadget.com>
2019-02-05 22:40:15 +0200

    Added recursive binary search

--- src/algo/binarysearch/App.java
+++ src/algo/binarysearch/App.java
@@ -3,8 +3,9 @@ package algo.binarysearch;
 public class App {
 
 	public static void main(String[] args) {
-		int[] a = {3, 5, 7, 9, 16, 28, 32, 47, 54, 67, 71, 83, 99, 121, 324, 213, 657, 435, 987, 768};
-		System.out.println(binarySearch(a, 7));
+		int[] a = {3, 5, 7, 9, 16, 28, 32, 47, 54, 67, 71, 83, 99, 121, 213, 324, 435, 657, 768, 987};
+//		System.out.println(binarySearch(a, 7));
+		System.out.println(recursiveBinarySearch(a, 0, a.length - 1, 71));
 	}
 	
 	public static int binarySearch(int[] a, int x) {
@@ -23,5 +24,20 @@ public class App {
 			}
 		}
 		return -1;
+	}
+	
+	public static int recursiveBinarySearch(int a[], int p, int r, int x) {
+		if (p > r) {
+			return -1;
+		} else {
+			int q = (p + r)/2;
+			if (a[q] == x) {
+				return q;
+			} else if (a[q] > x) {
+				return recursiveBinarySearch(a, p, q - 1, x);
+			} else {
+				return recursiveBinarySearch(a, q + 1, r, x);
+			}
+		}
 	}
 }
