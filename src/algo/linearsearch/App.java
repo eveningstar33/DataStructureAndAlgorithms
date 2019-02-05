3c181851a795ec3963712dbbd57dc432291292c7
“Dragos <contact@usdgadget.com>
2019-02-05 14:06:41 +0200

    Changed the package name

--- src/algo/linearsearch/App.java
+++ src/algo/linearsearch/App.java
@@ -1,10 +1,10 @@
-package algo.search;
+package algo.linearsearch;
 
 public class App {
 
 	public static void main(String[] args) {
 		int[] a = {3, 5, 1, 9, 6, 8, 2, 7, 4};
-		System.out.println(linearSearch(a, 4));
+		System.out.println(linearSearch(a, 2));
 	}
 	
 	public static int linearSearch(int[] a, int x) {
