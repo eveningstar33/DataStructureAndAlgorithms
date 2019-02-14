80fa4fde5138959b5bf4ee704b8f43f99d408ff7
“Dragos <contact@usdgadget.com>
2019-02-14 12:56:46 +0200

    Added circular queue implementation

---
+++ src/ds/circularqueue/App.java
@@ -0,0 +1,49 @@
+package ds.circularqueue;
+
+public class App {
+	public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+        Employee tomFord = new Employee("Tom", "Ford", 268);
+        
+        ArrayQueue queue = new ArrayQueue(5);
+        
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeWilson);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(tomFord);
+
+        queue.printQueue();
+        
+//        queue.add(janeJones);
+//        queue.add(johnDoe);
+//        queue.add(marySmith);
+//        queue.add(mikeWilson);
+//        queue.add(billEnd);
+////        queue.printQueue();
+//        
+//        queue.remove();
+//        queue.remove();
+////        queue.printQueue();
+//        
+////        System.out.println(queue.peek());
+////        queue.printQueue();
+//        
+//        queue.remove();
+//        queue.remove();
+//        queue.remove();
+//        
+//        queue.add(mikeWilson);
+//        
+//        queue.printQueue();
+	}
+}
