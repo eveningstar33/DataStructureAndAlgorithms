c4e8b5e6ece54d407dedc3515cc11b5d139e6c7b
“Dragos <contact@usdgadget.com>
2019-02-25 20:05:54 +0200

    Added linear probing to add and retrieve items

--- src/ds/hashtablesarray/App.java
+++ src/ds/hashtablesarray/App.java
@@ -8,16 +8,23 @@ public class App {
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
         Employee billEnd = new Employee("Bill", "End", 78);
+        Employee cacat = new Employee("Mars", "Cacat", 422);
+        Employee gaga = new Employee("Gaga", "Ertyu", 3647);
+        Employee paul = new Employee("Paul", "Bobos", 1920);
         
         SimpleHashtable ht = new SimpleHashtable();
         ht.put("Jones", janeJones);
         ht.put("Doe", johnDoe);
         ht.put("Wilson", mikeWilson);
         ht.put("Smith", marySmith);
+        ht.put("Cacat", cacat);
+        ht.put("Ertyu", gaga);
+        ht.put("Bobos", paul);
 
-//        ht.printHashtable();
+        ht.printHashtable();
         
         System.out.println("Retrieve key Wilson: " + ht.get("Wilson"));
+        System.out.println("Retrieve key Smith: " + ht.get("Smith"));
 	}
 
 }
--- src/ds/hashtablesarray/SimpleHashtable.java
+++ src/ds/hashtablesarray/SimpleHashtable.java
@@ -2,33 +2,85 @@ package ds.hashtablesarray;
 
 public class SimpleHashtable {
 
-	private Employee[] hashtable;
+	private StoredEmployee[] hashtable;
 	
 	public SimpleHashtable() {
-		hashtable = new Employee[10];
+		hashtable = new StoredEmployee[10];
 	}
 	
 	public void put(String key, Employee employee) {
 		int hashedKey = hashKey(key);
-		if (hashtable[hashedKey] != null) {
+		if (occupied(hashedKey)) {
+			int stopIndex = hashedKey;
+			hashedKey = incrementHashedKey(hashedKey);
+			
+			while (occupied(hashedKey) && hashedKey != stopIndex) {
+				hashedKey = (hashedKey + 1) % hashtable.length; 
+			}
+		}
+		if (occupied(hashedKey)) {
 			System.out.println("Sorry, there's already an employee at position " + hashedKey);
 		} else {
-			hashtable[hashedKey] = employee;
+			hashtable[hashedKey] = new StoredEmployee(key, employee);
 		}
 	}
 	
 	public Employee get(String key) {
-		int hashedKey = hashKey(key);
-		return hashtable[hashedKey];
+		int hashedKey = findKey(key);
+		if (hashedKey == -1) {
+			return null;
+		}
+		return hashtable[hashedKey].getEmployee();
 	}
 	
 	private int hashKey(String key) {
 		return key.length() % hashtable.length;
 	}
 	
+	private int findKey(String key) {
+		int hashedKey = hashKey(key);
+		if (hashtable[hashedKey] != null && hashtable[hashedKey].getKey().equals(key)) { 
+			return hashedKey;
+		}
+		
+		int stopIndex = hashedKey;
+		hashedKey = incrementHashedKey(hashedKey);
+		
+		while (hashedKey != stopIndex && 
+				hashtable[hashedKey] != null && 
+				!hashtable[hashedKey].getKey().equals(key)) {
+			hashedKey = (hashedKey + 1) % hashtable.length;
+		}
+		
+		if (hashtable[hashedKey] != null && 
+				hashtable[hashedKey].getKey().equals(key)) {
+			return hashedKey;
+		} else {
+			return -1;
+		}
+	}
+	
+	public int incrementHashedKey(int hashedKey) {
+		if (hashedKey == hashtable.length - 1) {
+			hashedKey = 0;
+		} else {
+			hashedKey++;
+		}
+		
+		return hashedKey;
+	}
+	
+	private boolean occupied(int index) {
+		return hashtable[index] != null; 
+	}
+	
 	public void printHashtable() {
 		for (int i = 0; i < hashtable.length; i++) {
-			System.out.println(hashtable[i]);
+			if (hashtable[i] == null) {
+				System.out.println("empty"); 
+			} else {
+				System.out.println("Position " + i + ": " + hashtable[i].getEmployee());
+			}
 		}
 	}
 }
